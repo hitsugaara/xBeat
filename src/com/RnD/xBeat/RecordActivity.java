ad5e84690549ed9d735eeb63f80a397b86d0b0d3
hitsugaara <rajatsharda@gmail.com>
2014-04-22 23:34:10 +0530

    Added Precount

--- src/com/RnD/xBeat/RecordActivity.java
+++ src/com/RnD/xBeat/RecordActivity.java
@@ -2,37 +2,27 @@ package com.RnD.xBeat;
 
 import java.io.IOException;
 
-
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-
-
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
-
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
-
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.CompoundButton;
 //import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.EditText;
-
-
 import android.widget.ToggleButton;
 import android.media.AudioManager;
-
-
 import android.media.MediaRecorder;
 import android.media.SoundPool;
 import android.os.Environment;
-
 
 import com.RnD.xBeat.R;
 //import com.RnD.xBeat.sequencer.Sequencer;
@@ -61,7 +51,9 @@ public class RecordActivity extends Activity {
 	private int bpmwhole;
 	EditText tempotext;
 	EditText indicator;
-	
+	Button Kick;
+	Button Snare;
+	Button Hat;
 	private static boolean recording;
 	
 	
@@ -83,9 +75,12 @@ public class RecordActivity extends Activity {
 		this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
 				WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		setContentView(R.layout.recordlayout);
-		Button Kick = (Button) findViewById(R.id.Kick);
-		Button Hat = (Button) findViewById(R.id.Hat);
-		Button Snare = (Button) findViewById(R.id.Snare);
+		Kick = (Button) findViewById(R.id.Kick);
+		Kick.setEnabled(false);
+		Hat = (Button) findViewById(R.id.Hat);
+		Hat.setEnabled(false);
+		Snare = (Button) findViewById(R.id.Snare);
+		Snare.setEnabled(false);
 		ToggleButton Record = (ToggleButton) findViewById(R.id.Record);
 		ToggleButton Beatbox = (ToggleButton) findViewById(R.id.Beatbox);
 		Button Tapper = (Button) findViewById(R.id.Tapper);
@@ -94,27 +89,32 @@ public class RecordActivity extends Activity {
 		sound = new SoundPool(1,AudioManager.STREAM_MUSIC,0);
 		sound.load(getBaseContext(), R.raw.hhc,1);
 		mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+		tempotext.setText(((Integer) 120).toString());//Default Tempo
 		
-		
-		
-	
 	
 	Record.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
 	    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
 	        if (isChecked) {
+	        	Kick.setEnabled(true);
+	        	Hat.setEnabled(true);
+	        	Snare.setEnabled(true);
 	        	settempo(Integer.parseInt(tempotext.getText().toString()));
-	        	
+	        	NewThread2 nn1= new NewThread2();
+	        	try {
+					nn1.t1.join();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 	        	NewThread n1 = new NewThread("Record");
-	        	
-	        	
 	        	Log.e(TAG,"Post-Rercod");
-	        	
 	        	NewThread n2 = new NewThread("Flash");
-	            
-	        	
 	            
 	        } else {
 	            stopRecording();
+	            Kick.setEnabled(false);
+	            Hat.setEnabled(false);
+	            Snare.setEnabled(false);
 	        }
 	    }
 	});
@@ -129,9 +129,6 @@ public class RecordActivity extends Activity {
 	});
 		
 	}
-	
-	
-	
 	
 	private void startRecording() {
 		Log.e(TAG,"In record");
@@ -148,8 +145,7 @@ public class RecordActivity extends Activity {
 			Log.e(TAG, "Couldn't prepare and start MediaRecorder");
 		}
 
-		mRecorder.start();
-		
+		mRecorder.start();	
 	}
 	
 	private void stopRecording() {
@@ -163,8 +159,6 @@ public class RecordActivity extends Activity {
 	
 	private void Tap(){
 		{
-			 
-			  
 			  long msecs = System.currentTimeMillis();
 			  if ((msecs - msecsprevious) > 2000)
 			    {
@@ -198,14 +192,10 @@ public class RecordActivity extends Activity {
 	
 	public void flash() 
 	{
-       
-		
 		 Handler refresh = new Handler(Looper.getMainLooper());
 			refresh.post(new Runnable() {
 			    public void run()
-			    {
-			    	
-					     
+			    {  
 					     indicator.setText(((Integer) (count2+1)).toString());
 					     Log.i(TAG,"Print");
 					     
@@ -213,13 +203,21 @@ public class RecordActivity extends Activity {
 		    });
 			return;
                 }
+	
+	private void precount()
+	{
+		
+		Handler refresh = new Handler(Looper.getMainLooper());
+		refresh.post(new Runnable() {
+		    public void run()
+		    {  
+		indicator.setText(("Pre").toString());
+		
+		    }
+		    });
+		return;
+		    }
                 	
-	   
-    
-	
-	
-	
-	
 	class NewThread implements Runnable
 	{
 		
@@ -241,13 +239,11 @@ public class RecordActivity extends Activity {
         	   // Start the thread
         }
 
-
 		@Override
 		public void run() {
 		
 			if(!b)
-			{
-				
+			{				
 				startRecording();
 			}
 			
@@ -260,15 +256,7 @@ public class RecordActivity extends Activity {
 				recording=true;
 				while (recording)
                 {
-					Log.e(TAG, "Boo");
-                	
-					
-
-					    
-
-					    
-					    
-					     
+					Log.e(TAG, "Boo");		     
 					millis = System.currentTimeMillis();
 				     
                     flash();  //indicator.setText(((Integer) count2).toString());
@@ -284,14 +272,44 @@ public class RecordActivity extends Activity {
 						}
 					    }
 					
-                
+			}
+		}
+	}
+	
+	class NewThread2 implements Runnable
+	{
+Thread t1;
+
+      NewThread2() 
+     {
+   
+       t1 = new Thread(this); 
+	   t1.start();
+    }
+
+		@Override
+		public void run() {
+			
+			precount();
+			for(int i=0;i<4;i++)
+			{
+				
+				millis = System.currentTimeMillis();
+			     
+	            
+			     
+			     long next = (60 * 1000) / bpm;
+			     sound.play(1,100, 100, 1, 0, 1);
+	       
+				     try {
+						Thread.sleep(next - (System.currentTimeMillis() - millis));
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
 			}
 		}
 		
-		
 	}
 	
-	
 		}
-
-
